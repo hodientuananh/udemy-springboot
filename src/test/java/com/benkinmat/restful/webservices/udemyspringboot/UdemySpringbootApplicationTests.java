4879d3f4563723d71d4357bc90e5e705cde7b4d9
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-02 10:10:39 +0700

    initial spring boot project

---
+++ src/test/java/com/benkinmat/restful/webservices/udemyspringboot/UdemySpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.benkinmat.restful.webservices.udemyspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemySpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
