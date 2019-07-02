b64a2abf9e304a6a1a4f753fb8652299cc16ed57
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-02 16:50:08 +0700

    create service for localhost

--- src/main/java/com/benkinmat/restful/webservices/udemyspringboot/HelloWorldController.java
+++ src/main/java/com/benkinmat/restful/webservices/udemyspringboot/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.benkinmat.restful.webservices.udemyspringboot;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
@@ -14,7 +16,8 @@ public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World");
+//		throw new RuntimeException("Some error has happened! Contact Support");
+		return new HelloWorldBean("Hello World - Changed");
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
