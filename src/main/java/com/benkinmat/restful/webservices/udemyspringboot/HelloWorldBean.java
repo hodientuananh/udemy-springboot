33532ee7e2ff146a01288860bb7f6f4027e4974d
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-02 10:47:08 +0700

    simple hello world bean

---
+++ src/main/java/com/benkinmat/restful/webservices/udemyspringboot/HelloWorldBean.java
@@ -0,0 +1,20 @@
+package com.benkinmat.restful.webservices.udemyspringboot;
+
+public class HelloWorldBean {
+	
+	private String message;
+	
+	public HelloWorldBean(String message) {
+		this.setMessage(message);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
--- src/main/java/com/benkinmat/restful/webservices/udemyspringboot/HelloWorldController.java
+++ src/main/java/com/benkinmat/restful/webservices/udemyspringboot/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.benkinmat.restful.webservices.udemyspringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +9,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 }
