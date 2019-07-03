3db869975d17e20399745dde35f59cd223291dcd
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-03 13:59:24 +0700

    create todo update call

--- src/main/java/com/benkinmat/restful/webservices/udemyspringboot/todo/TodoHardCodedService.java
+++ src/main/java/com/benkinmat/restful/webservices/udemyspringboot/todo/TodoHardCodedService.java
@@ -13,12 +13,36 @@ public class TodoHardCodedService {
 	private static int idCounter = 0;
 	
 	static {
-		todos.add(new Todo(++ idCounter, "benkinmat", "Learn Angular", new Date(), false));
+		todos.add(new Todo(++ idCounter, "benkinmat", "Learn Angular", new Date(), true));
 		todos.add(new Todo(++ idCounter, "benkinmat", "Learn Spring Boot", new Date(), false));
 		todos.add(new Todo(++ idCounter, "benkinmat", "Learn Database", new Date(), false));
 	}
 
 	public List<Todo> findAll(){
 		return todos;
+	}
+	
+	public Todo deleteById(long id) {
+		Todo todo = findById(id);
+		
+		if (todo == null) 
+			return null;
+		
+		if (todos.remove(todo)) {
+			return todo;
+		};
+		
+		return null;
+	}
+
+	public Todo findById(long id) {
+		// TODO Auto-generated method stub
+		for (Todo todo: todos) {
+			if (todo.getId() == id) {
+				return todo;
+			}
+		}
+		
+		return null;
 	}
 }
--- src/main/java/com/benkinmat/restful/webservices/udemyspringboot/todo/TodoResource.java
+++ src/main/java/com/benkinmat/restful/webservices/udemyspringboot/todo/TodoResource.java
@@ -3,10 +3,14 @@ package com.benkinmat.restful.webservices.udemyspringboot.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TodoResource {
 	
@@ -18,4 +22,19 @@ public class TodoResource {
 		return todoService.findAll();
 	}
 
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+		Todo todo = todoService.deleteById(id);
+		
+		if (todo != null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();		
+	}
+	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo getTodo(@PathVariable String username, @PathVariable long id){
+		return todoService.findById(id);
+	}
 }
